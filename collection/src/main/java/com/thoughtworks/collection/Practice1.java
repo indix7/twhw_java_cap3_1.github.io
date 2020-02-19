93fff0451b15ca9ead6e20478cdcbaee84992578
maoxin <indix7@outlook.com>
2020-02-19 12:13:18 +0800

    20200219

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +13,11 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        HashSet<Integer> hashSet = new HashSet<>();
+        Random random = new Random();
+        while (hashSet.size() < 10) {
+            hashSet.add(random.nextInt(20));
+        }
+        return hashSet;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,19 +1,48 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Iterator;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        long t1 = System.currentTimeMillis();
+        List<Integer> list = getNumbers();
+        forLoop(list);
+        forEachLoop(list);
+        iterateLoop(list);
+        System.out.println(System.currentTimeMillis()-t1);  //ArrayList 34, LinkedList 13
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i <= 100; i++) {
+            list.add(i);
+        }
+        return list;
     }
 
+    public static void forLoop(List<Integer> list) {
+        for (int index = 0; index < list.size(); index ++) {
+            System.out.println(list.get(index));
+        }
+    }
 
+    public static void forEachLoop(List<Integer> list) {
+        for (int element: list) {
+            System.out.println(element);
+        }
+    }
+
+    public static void iterateLoop(List<Integer> list) {
+        for(Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
+            System.out.println(iterator.next());
+        }
+    }
 }
